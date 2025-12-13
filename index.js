3d8d525235458c1c9a2f88d71a6603527ec82866
Hanzla2341 <hanzlasabir006@gmail.com>
2025-12-13 05:27:01 -0800

    alll

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const connectDB = require('./config/db.js');
-const chatbotmessege = require('./routes/chatbot.route.js');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("../config/db.js");
+const chatbotRoutes = require("../routes/chatbot.route.js");
 
 dotenv.config();
 
 const app = express();
 
-// 🔥 Connect DB ONCE at startup
-connectDB();
-
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Chatbot backend running ✔'));
-app.use('/api', chatbotmessege);
+// Connect DB safely
+let isConnected = false;
+async function connectOnce() {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+}
 
-// Start server
-module.exports=app;
+// Wrap all requests
+app.use(async (req, res, next) => {
+  await connectOnce();
+  next();
+});
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Chatbot backend running ✔");
+});
+
+app.use("/api", chatbotRoutes);
+
+module.exports = app;
